beade5bb173474cb8352dc35026e1c020b737993
Michael <michaelyoung451@gmail.com>
2020-03-07 15:17:25 -0600

    Thymeleaf, Forms, and Bootstrap

--- src/main/java/com/example/codingevents/controllers/EventController.java
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package com.example.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,22 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String events (Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Planet Comicon");
-        events.add("ConQuest");
-        events.add("NakaCon");
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEvent() {
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 }
