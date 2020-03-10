c618f3bd372fcef88062d2ad3322aab77c23a4bd
Michael <michaelyoung451@gmail.com>
2020-03-09 20:25:50 -0500

    Add Delete Event functionality

--- src/main/java/com/example/codingevents/controllers/EventController.java
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package com.example.codingevents.controllers;
 
+import com.example.codingevents.data.EventData;
 import com.example.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +16,9 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
-
     @GetMapping
     public String events (Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -29,8 +28,26 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+    public String ProcessCreateEventsForm(@RequestParam String eventName,
+                                          @RequestParam String eventDescription) {
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String ProcessDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
     }
 }
